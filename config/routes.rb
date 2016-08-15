efa2edd2629ee629282329114cedb8a89c71ed39
Jabox <jasonadams023@gmail.com>
2016-08-15 10:56:57 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	root 'static#welcome'
-  get 'static/welcome'
+  	get 'static/welcome'
 
-  devise_for :users
+  	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
