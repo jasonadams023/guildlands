21c1812a20af6126ed757f8d06644add016026db
Jabox <jasonadams023@gmail.com>
2016-08-17 12:07:31 -0500

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,19 @@ locations = Location.create([{name: 'Lesure', description: 'A peaceful kingdom, 
 
 guild_halls = GuildHall.create([{name: 'First Hall', size: 20, unit_limit: 10, effects: {activities: 'inn'}, guild_id: 1, location_id: 1},
 								{name: 'Second', size: 10, unit_limit: 5, effects: {}, guild_id: 2, location_id: 2},
-								{name: 'Third', size: 30, unit_limit: 10, effects: {activities: 'hospital'} guild_id: 2, location_id: 1}])
+								{name: 'Third', size: 30, unit_limit: 10, effects: {activities: 'hospital'}, guild_id: 2, location_id: 1}])
+
+activities = Activity.create([{name: 'Idle', description: 'This unit is not currently doing anything.', effects: {stats: 'heal'}},
+								{name: 'Gather', description: 'Go out and gather supplies from the surrounding area.', effects: {inventory: 'add_regional_stamina'}, location_id: 1},
+								{name: 'Gather', description: 'Go out and gather supplies from the surrounding area.', effects: {inventory: 'add_regional_stamina'}, location_id: 2}])
+
+units = Unit.create([{name: 'Ulbert', total_xp: 1000, spent_xp: 0, hiring_cost: 0, upkeep_cost: 10,
+						max_hp: 50, current_hp: 50, max_sp: 50, current_sp: 50,
+						strength: 5, agility: 5, vitality: 5, stamina: 5, intelligence: 5, focus: 5,
+						dodge: 5, resilience: 0, resist: 5,
+						effects: {},
+						guild_id: 1, guild_hall_id: 1, location_id: 1, activity_id: 1}])
+
+unit_abilities = UnitAbility.create([{name: 'Gatherer', description: 'This unit is better at gathering', xp_cost: 50, category: 'passive', ap_cost: 0, sp_cost: 0, effects: {gathering: 'doubled'}}])
+
+items = Item.create([{name: 'potion', description: 'Restores 15 hp.', effects: {current_hp: '+15'}, category: 'consumable', value: 50}])
