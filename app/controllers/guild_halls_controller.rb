314284b9b7eb7688f4e5d958524bc915e8477d5a
Jabox <jasonadams023@gmail.com>
2016-08-17 21:45:49 -0500

    Add guild hall creation

--- app/controllers/guild_halls_controller.rb
+++ app/controllers/guild_halls_controller.rb
@@ -1,10 +1,36 @@
 class GuildHallsController < ApplicationController
-  def show
-  end
+	def index
+	end
 
-  def new
-  end
+	def show
+		@hall = GuildHall.find(params[:id])
+	end
 
-  def edit
-  end
+	def new
+		@guild = current_user.guild
+		@hall = GuildHall.new
+	end
+
+	def create
+    	@hall = GuildHall.new(guild_hall_params)
+
+		if @hall.save
+			redirect_to current_user.guild
+		else
+			flash[:alert] = "Failed to create Guild Hall."
+			@guild = current_user.guild
+			@hall = GuildHall.new
+			render :edit
+		end
+    end
+
+	def edit
+		@guild = current_user.guild
+		@hall = GuildHall.new
+	end
+
+	private
+		def guild_hall_params
+			params.require(:guild_hall).permit(:name, :size, :unit_limit, :effects, :guild_id, :location_id)
+		end
 end
