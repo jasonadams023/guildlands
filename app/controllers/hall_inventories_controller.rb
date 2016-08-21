4eb08033a9e03b3162fa2137ab8c71baa3b3a1e3
Jabox <jasonadams023@gmail.com>
2016-08-21 13:41:24 -0500

    Add move items functionality

--- app/controllers/hall_inventories_controller.rb
+++ app/controllers/hall_inventories_controller.rb
@@ -27,6 +27,38 @@ class HallInventoriesController < ApplicationController
 	def edit
 	end
 
+	def update
+		inventory = HallInventory.find(params[:id])
+		hall = GuildHall.find(params[:hall_inventory][:guild_hall_id])
+		amount = params[:hall_inventory][:available].to_i
+
+		if params[:move] == 'true'
+			if !(hall.items.include?(inventory.item)) #If inventory does not exist in target hall
+				hall.items << inventory.item #Initialize new inventory
+				id = hall.hall_inventories.find_index{|i| inventory.item_id == i.item_id}
+				hall.hall_inventories[id].total = 0
+				hall.hall_inventories[id].available = 0
+				hall.hall_inventories[id].selling = 0 #/Initialize new inventory
+			else
+				id = hall.hall_inventories.find_index{|i| inventory.item_id == i.item_id}
+			end
+			
+			hall.hall_inventories[id].total += amount #update target hall
+			hall.hall_inventories[id].available += amount
+
+			inventory.total -= amount #update origin hall
+			inventory.available -= amount
+
+			if inventory.save && hall.save && hall.hall_inventories[id].save
+				flash[:notice] = "Succesfully moved items."
+			else
+				flash[:alert] = "Failed to update."
+			end
+		end
+
+		redirect_to inventory
+	end
+
 	def create
 	end
 
