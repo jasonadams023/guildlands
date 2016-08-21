5e62b4504ebbb25dfded34f09e7bb970d4c22294
Jabox <jasonadams023@gmail.com>
2016-08-21 15:31:20 -0500

    Add unit ability views and purchasing

--- app/controllers/guild_abilities_controller.rb
+++ app/controllers/guild_abilities_controller.rb
@@ -4,29 +4,6 @@ class GuildAbilitiesController < ApplicationController
 		@guild = current_user.guild
 	end
 
-	def purchase
-		ability = GuildAbility.find(params[:id])
-		guild = current_user.guild
-		rep = guild.total_rep - guild.spent_rep
-		
-
-
-		if rep >= ability.rep_cost
-			guild.guild_abilities << ability
-			guild.spent_rep += ability.rep_cost
-
-			if guild.save
-				flash[:notice] = "Ability purchased."
-			else
-				flash[:alert] = "Failed to purchase ability."
-			end
-		else
-			flash[:alert] = "Not enough reputation to purchase ability."
-		end
-
-		redirect_to guild_path(guild)
-	end
-
 	def show
 		@ability = GuildAbility.find(params[:id])
 	end
@@ -38,6 +15,32 @@ class GuildAbilitiesController < ApplicationController
 	end
 
 	def create
+	end
+
+	def purchase
+		ability = GuildAbility.find(params[:id])
+		guild = current_user.guild
+		rep = guild.total_rep - guild.spent_rep
+		
+		if rep >= ability.rep_cost
+			if guild.money >= ability.rep_cost
+				guild.money -= rep_cost
+
+				guild.guild_abilities << ability
+				guild.spent_rep += ability.rep_cost
+
+				if guild.save
+					flash[:notice] = "Ability purchased."
+				else
+					flash[:alert] = "Failed to purchase ability."
+				end
+			else
+				flash[:alert] = "Guild does not have enough money to purchase this ability."
+		else
+			flash[:alert] = "Not enough reputation to purchase ability."
+		end
+
+		redirect_to guild_path(guild)
 	end
 
 	def release
--- app/controllers/unit_abilities_controller.rb
+++ app/controllers/unit_abilities_controller.rb
@@ -1,10 +1,50 @@
 class UnitAbilitiesController < ApplicationController
-  def show
-  end
+	def index
+		@abilities = UnitAbility.all
+		if params[:unit_id] != nil then @unit = Unit.find(params[:unit_id]) end
+	end
 
-  def new
-  end
+	def show
+		@ability = UnitAbility.find(params[:id])
+		if params[:unit_id] != nil then @unit = Unit.find(params[:unit_id]) end
+	end
 
-  def edit
-  end
+	def new
+	end
+
+	def edit
+	end
+
+	def purchase
+		ability = UnitAbility.find(params[:id])
+		unit = Unit.find(params[:unit_id])
+		guild = unit.guild_hall.guild
+
+		if (unit.total_xp - unit.spent_xp) >= ability.xp_cost
+			if guild.money >= ability.xp_cost
+				unit.unit_abilities << ability
+				unit.spent_xp += ability.xp_cost
+
+				guild.money -= ability.xp_cost
+
+				if unit.save && guild.save
+					flash[:notice] = "Ability training purchased."
+				else
+					flash[:alert] = "Failed to update."
+				end
+			else
+				flash[:alert] = "Guild does not have enough money to train this ability."
+			end
+		else
+			flash[:alert] = "This unit does not have enough experience available to train this ability."
+		end
+
+		redirect_to unit		
+	end
+
+	def release
+	end
+
+	def destroy
+	end
 end
