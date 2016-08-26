e072145988e4daab003ea2057f87a6e01d4ed090
Jabox <jasonadams023@gmail.com>
2016-08-25 20:04:54 -0500

    Add unit inventory show and return

--- app/controllers/unit_inventories_controller.rb
+++ app/controllers/unit_inventories_controller.rb
@@ -1,9 +1,17 @@
 class UnitInventoriesController < ApplicationController
+	def index
+		@unit_inventories = UnitInventory.select{|inv| inv.unit_id == params[:unit_id].to_i && inv.amount > 0}
+	end
+
+	def show
+		@unit_inventory = UnitInventory.find(params[:id])
+	end
+
 	def create
 		unit_inventory = UnitInventory.new(unit_inventory_params)
 
 		if unit_inventory.unit.unit_inventories.select{|inv| inv.hall_inventory.item_id == unit_inventory.hall_inventory.item_id}.length != 0
-			unit_inventory = unit_inventory.unit.unit_inventories.select{|inv| inv.item_id == unit_inventory.item_id}[0]
+			unit_inventory = unit_inventory.unit.unit_inventories.select{|inv| inv.hall_inventory.item_id == unit_inventory.hall_inventory.item_id}[0]
 			unit_inventory.amount += params[:unit_inventory][:amount].to_i
 		end
 
@@ -14,6 +22,27 @@ class UnitInventoriesController < ApplicationController
 			flash[:notice] = "Succesfully added items to unit inventory."
 		else
 			flash[:alert] = "Failed to update."
+		end
+
+		redirect_to unit_inventory.unit
+	end
+
+	def update
+		if params[:return] == 'true'
+			unit_inventory = UnitInventory.find(params[:id])
+			amount = params[:unit_inventory][:amount].to_i
+
+			unit_inventory.amount -= amount
+			if unit_inventory.amount == 0 then unit_inventory.equipped = false end
+
+			unit_inventory.hall_inventory.available += amount
+			unit_inventory.hall_inventory.using -= amount
+
+			if unit_inventory.save && unit_inventory.hall_inventory.save && unit_inventory.unit.save
+				flash[:notice] = "Items moved back to Guild Hall."
+			else
+				flash[:alert] = "Failed to update."
+			end
 		end
 
 		redirect_to unit_inventory.unit
