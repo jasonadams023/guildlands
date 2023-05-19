c92abe8d59b601c3247ae71806195cf391c85a57
Jason Adams <jason@spacee.com>
2023-05-19 11:29:29 -0500

    Removes case_sensitive validation

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,4 +1,4 @@
 class ChatRoom < ApplicationRecord
 	has_and_belongs_to_many :users
-	validates :name, presence: true, uniqueness: true, case_sensitive: false
+	validates :name, presence: true, uniqueness: true
 end
